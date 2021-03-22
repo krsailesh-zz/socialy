681f7631b5489a4e6945a94f2e9378a6d760c491
krsailesh <krsailesh2001@gmail.com>
2021-03-22 16:09:17 +0530

    path in app.js changed

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(require('./router/user'))
 
 if(process.env.NODE_ENV=='production'){
     app.use(express.static('/client/build'))
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    app.get('/*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'build','index.html'))
     })
 }
 
