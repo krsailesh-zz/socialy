9f052797e6dc3adb1214d9afeb37e68dd959fd3d
krsailesh <krsailesh2001@gmail.com>
2021-03-23 18:23:20 +0530

    static file path edited

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(require('./router/post'))
 app.use(require('./router/user'))
 
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('/client/build'))
+    app.use(express.static(__dirname+'/client/build'))
     app.get('/*', (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.join(__dirname,'client/build','index.html'))
     })
 }
 
