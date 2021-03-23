a212aced5b5c86a6bdd8fb81c41c17f95271fe67
krsailesh <krsailesh2001@gmail.com>
2021-03-23 17:28:12 +0530

    procfile added

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(require('./router/auth'))
 app.use(require('./router/post'))
 app.use(require('./router/user'))
 
-if(process.env.NODE_ENV=='production'){
+if(process.env.NODE_ENV==='production'){
     app.use(express.static('/client/build'))
     app.get('/*', (req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
